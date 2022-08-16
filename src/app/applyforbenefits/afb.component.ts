6f6cd4ba142818a853528a40a33ec8b82ec4452d
SaminathanG <geeyes.02@gmail.com>
2022-08-16 12:29:45 -0700

    updated 	modified:   src/app/applyforbenefits/afb.component.html 	modified:   src/app/applyforbenefits/afb.component.scss 	modified:   src/app/applyforbenefits/afb.component.ts

--- src/app/applyforbenefits/afb.component.ts
+++ src/app/applyforbenefits/afb.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-afb',
@@ -13,6 +14,11 @@ export class AfbComponent implements OnInit {
     loading = false;
     submitted = false;
     formBuilder: any;
+    @ViewChild('sidenav') sidenav: MatSidenav | undefined;
+    isExpanded = true;
+    showSubmenu: boolean = false;
+    isShowing = false;
+    showSubSubMenu: boolean = false;
 
     constructor(private router: Router) {
     }
@@ -21,7 +27,24 @@ export class AfbComponent implements OnInit {
     }
 
     back(): void{
-        
+        this.router.navigate(['']);
+    }
+
+    mouseenter() {
+      if (!this.isExpanded) {
+        this.isShowing = true;
+      }
+    }
+    
+    mouseleave() {
+      if (!this.isExpanded) {
+        this.isShowing = false;
+      }
+    }
+
+    // tslint:disable-next-line:typedef
+    onEdit(){
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
     }
 
 }
